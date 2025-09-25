60bff35b822f6095fbf4e74d14d9b54ca3012c58
Daniel-Lidman <dlidman@gmail.com>
2025-09-25 12:25:13 -0500

    Remove Dark / Light Mode bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="not-dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-800`}
       >
